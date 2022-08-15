52e3ca3e13f6df5f250a4237f3049b7f1626fd05
Gabriel Costa <gacosil0204@gmail.com>
2022-08-15 11:04:30 -0300

    Initializing Services

---
+++ GreetingService/src/test/java/br/com/big/GreetingService/GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.big.GreetingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
