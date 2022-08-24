0747681f474d22ef8d91fa8ebef6531cf3c2f220
Gabriel Costa <gacosil0204@gmail.com>
2022-08-24 16:26:40 -0300

    OpenApiUi implemented

--- OtherBook/src/main/java/br/com/big/controller/BookController.java
+++ OtherBook/src/main/java/br/com/big/controller/BookController.java
@@ -1,20 +1,19 @@
 package br.com.big.controller;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.big.model.Book;
 import br.com.big.proxy.CambioProxy;
 import br.com.big.repository.BookRepository;
-import br.com.big.response.Cambio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -28,33 +27,7 @@ public class BookController {
 	@Autowired
 	private CambioProxy proxy;
 	
-	/*@GetMapping(value = "/{id}/{currency}")
-	public Book findBook(
-			@PathVariable("id") Long id,
-			@PathVariable("id") String currency
-			) {
-	
-		var book = repository.getById(id);
-		if(book == null) throw new RuntimeException("Book not found");
-		
-		HashMap<String, String> params = new HashMap<>();
-		
-		params.put("amount", book.getPrice().toString());
-		params.put("from", "USD");
-		params.put("to", currency);
-		
-		var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}", 
-				Cambio.class, 
-				params);
-		
-		var cambio = response.getBody();
-		
-		
-		var port = environment.getProperty("local.server.port");
-		book.setEnvironment(port);
-		book.setPrice(cambio.getConvertedValue());
-		return book;
-	}*/
+	@Operation(summary = "Find a specific book by your ID")
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(
 			@PathVariable("id") Long id,
